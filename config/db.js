f9e50c5a4bd524350bf90fd30da829b34d4a4940
ashik13a <ashik74a@gmail.com>
2026-09-12 20:16:48 +0600

    use mongodb atlas

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-const connectDB = async()=>{
-  try{
-    await mongoose.connect("mongodb://127.0.0.1:27017/note-taking-app");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB Connected!");
   } catch (error) {
-    console.log("MongoDB Connection Failed: ", error.message);
+    console.log("MongoDB Connection Failed:", error.message);
     process.exit(1);
-
   }
 };
 
