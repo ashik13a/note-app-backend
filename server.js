dce06a225cae75ec84ec67e2f8288aee3b624ca1
ashik13a <ashik74a@gmail.com>
2026-09-12 20:11:05 +0600

    configure server for render

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 
@@ -6,39 +8,24 @@ const noteRoutes = require("./routes/noteRoutes");
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// ==============================
-// CONNECT MONGODB
-// ==============================
-
+// Connect MongoDB
 connectDB();
 
-// ==============================
-// MIDDLEWARE
-// ==============================
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// ==============================
-// HOME ROUTE
-// ==============================
-
+// Home route
 app.get("/", (req, res) => {
   res.send("We are open, looking for sotruzz");
 });
 
-// ==============================
-// NOTE ROUTES
-// ==============================
-
+// Note routes
 app.use("/api/notes", noteRoutes);
 
-// ==============================
-// START SERVER
-// ==============================
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+// Start server
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
